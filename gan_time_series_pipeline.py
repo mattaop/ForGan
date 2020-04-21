Fixed validation error computation

gan_time_series_pipeline.py
@@ -98,7 +98,7 @@ def train_gan(gan, data, epochs, batch_size=128, discriminator_epochs=1):
     return gan, validation_error
 
 
-def test_model(gan, data, validation_error, mc_forward_passes=500):
+def test_model(gan, data, validation_error, mc_forward_passes=1000):
     forecast = gan.monte_carlo_forecast(data, int(len(data)-gan.window_size), mc_forward_passes)  # steps x horizon x mc_forward_passes
     forecast_mean = forecast.mean(axis=-1)
     forecast_std = forecast.std(axis=-1)
@@ -116,7 +116,8 @@ def test_model(gan, data, validation_error, mc_forward_passes=500):
                      alpha=0.2, edgecolor='#CC4F1B', facecolor='#FF9848', label='95%-PI')
     plt.legend()
     plt.show()
-    print('Forecast error:', mean_squared_error(data[gan.window_size:], forecast_mean[:, 0]))
+    print('Forecast MSE:', mean_squared_error(data[gan.window_size:], forecast_mean[:, 0]))
+    print('Validation MSE:', validation_error)
     print('Mean forecast standard deviation:', forecast_std.mean(axis=0))
     print('Mean total forecast standard deviation:', total_uncertainty.mean(axis=0))
     print('80%-prediction interval coverage:', compute_coverage(actual_values=data[gan.window_size:],
@@ -133,7 +134,7 @@ def pipeline():
     cfg = load_config_file('config\\config.yml')
     gan = configure_model(model_name=cfg['gan']['model_name'])
     train, test = load_data(cfg=cfg['data'], window_size=gan.window_size)
-    trained_gan, validation_error = train_gan(gan=gan, data=train, epochs=500, batch_size=256, discriminator_epochs=2)
+    trained_gan, validation_error = train_gan(gan=gan, data=train, epochs=10000, batch_size=256, discriminator_epochs=1)
     test_model(gan=trained_gan, data=test, validation_error=validation_error, mc_forward_passes=500)
 
 

models/recurrent_gan/RecurrentConvGAN.py
@@ -64,11 +64,13 @@ class RecurrentConvGAN(GAN):
         noise_inp = Input(shape=noise_shape)
         historic_inp = Input(shape=historic_shape)
 
-        hist = SimpleRNN(32, return_sequences=False)(historic_inp)
+        hist = LSTM(64, return_sequences=True)(historic_inp)
+        hist = LSTM(64, return_sequences=False)(hist)
+
         # hist = ReLU()(hist)
 
         x = Concatenate(axis=1)([hist, noise_inp])
-        x = BatchNormalization()(x)
+        # x = BatchNormalization()(x)
         x = Dense(32)(x)
         x = ReLU()(x)
         prediction = Dense(self.forecasting_horizon)(x)
@@ -271,8 +273,8 @@ class RecurrentConvGAN(GAN):
         forecast = np.zeros([steps, self.forecasting_horizon, mc_forward_passes])
         for i in tqdm(range(steps)):
             for j in range(mc_forward_passes):
-                noise = self._generate_noise(1)
-                forecast[i, :, j] = self.generator.predict([time_series[:, i:self.window_size + i], noise])[0]
+                generator_noise = self._generate_noise(1)
+                forecast[i, :, j] = self.generator.predict([time_series[:, i:self.window_size + i], generator_noise])[0]
         if plot:
             plt.figure()
             plt.plot(np.linspace(1, len(data[0]), len(data[0])), data[0], label='real data')