6a38d4ec95f6ba292abef98ec914731d05ce24f8
mattaop <snowboard95@live.no>
2020-05-04 14:15:36 +0200

    Added hybrid models and pipelines for executing hybrid models

--- hybrid_time_series_pipeline.py
+++ hybrid_time_series_pipeline.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['PYTHONHASHSEED'] = '0'
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -8,6 +9,7 @@ os.environ['TF_DETERMINISTIC_OPS'] = '1'
 import numpy as np
 import random as rn
 import tensorflow as tf
+
 print(tf.__version__)
 seed = 4
 rn.seed(seed)
@@ -15,6 +17,7 @@ np.random.seed(seed)
 tf.set_random_seed(seed)
 
 from keras import backend as k
+
 config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1,
                         allow_soft_placement=True, device_count={'CPU': 1})
 sess = tf.Session(graph=tf.get_default_graph(), config=config)
@@ -54,8 +57,8 @@ def load_data(cfg, window_size):
     else:
         return None
     print('Data shape', data.shape)
-    train = data[:-int(len(data)*cfg['test_split'])]
-    test = data[-int(len(data)*cfg['test_split']+window_size):]
+    train = data[:-int(len(data) * cfg['test_split'])]
+    test = data[-int(len(data) * cfg['test_split'] + window_size):]
     train, test = scale_data(train, test)
     return train, test
 
@@ -80,29 +83,12 @@ def plot_results(y, label, y2=None, y2_label=None, title='', y_label='Value'):
     plt.show()
 
 
-def compute_validation_error(gan, data):
-    # Split validation data into (x_t-l, ..., x_t), (x_t+1) pairs
-    x_val, y_val = split_sequence(data, gan.window_size, gan.output_size)
-
-    # Compute inherent noise on validation set
-    y_predicted = gan.forecast(x_val)
-
-    validation_mse = np.zeros(gan.output_size)
-    for i in range(gan.output_size):
-        validation_mse[i] = mean_squared_error(y_val[:, i], y_predicted[:, i])
-    return validation_mse
-
-
 def train_gan(gan, data, epochs, batch_size=128, verbose=1):
-    # Split data in training and validation set
-    train, val = data[:-int(len(data)*0.1)], data[-int(gan.window_size+len(data)*0.1):]
-
     # Split training data into (x_t-l, ..., x_t), (x_t+1) pairs
-    x_train, y_train = split_sequence(train, gan.window_size, gan.output_size)
+    x_train, y_train = split_sequence(data, gan.window_size, gan.output_size)
 
     history = gan.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
 
-    validation_mse = compute_validation_error(gan, val)
 
     """
     plt.figure()
@@ -110,7 +96,7 @@ def train_gan(gan, data, epochs, batch_size=128, verbose=1):
     plt.plot(np.linspace(1, epochs, epochs), history['mse'], label='Forecast MSE generator')
     plt.legend()
     # plt.show()
-    
+
     plt.figure()
     plt.title('Training Generator and Discriminator Loss')
     plt.plot(np.linspace(1, epochs, epochs), history['G_loss'], label='Generator loss')
@@ -119,33 +105,34 @@ def train_gan(gan, data, epochs, batch_size=128, verbose=1):
     # plt.show()
     """
 
-    return gan, validation_mse
+    return gan
 
 
-def test_model(gan, data, validation_mse, plot=True):
-    forecast = gan.monte_carlo_forecast(data, steps=int(len(data)-gan.window_size))  # steps x horizon x mc_forward_passes
+def test_model(gan, data, plot=True):
+    forecast = gan.monte_carlo_forecast(data,
+                                        steps=int(len(data) - gan.window_size))  # steps x horizon x mc_forward_passes
     forecast_mean = forecast.mean(axis=-1)
     forecast_std = forecast.std(axis=-1)
+    print('Forecast SD:', forecast_std)
     forecast_var = forecast.var(axis=-1)
-    print('Forecast std', forecast_std)
-
+    total_uncertainty = np.sqrt(forecast_var)
     # print('Mutual information:', normalized_mutual_info_score(forecast_mean[:, 0], data[gan.window_size:, 0]))
-    total_uncertainty = np.sqrt(forecast_var + validation_mse)
     if plot:
-        x_pred = np.linspace(gan.window_size+1, len(data), len(data)-gan.window_size)
+        x_pred = np.linspace(gan.window_size + 1, len(data), len(data) - gan.window_size)
         plt.figure()
         plt.plot(np.linspace(1, len(data), len(data)), data, label='Data')
         plt.plot(x_pred, forecast_mean[:, 0], label='Predictions')
-        plt.fill_between(x_pred, forecast_mean[:, 0]-1.28*forecast_std[:, 0], forecast_mean[:, 0]+1.28*forecast_std[:, 0],
+        plt.fill_between(x_pred, forecast_mean[:, 0] - 1.28 * forecast_std[:, 0],
+                         forecast_mean[:, 0] + 1.28 * forecast_std[:, 0],
                          alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848', label='80%-PI')
-        plt.fill_between(x_pred, forecast_mean[:, 0]-1.96*forecast_std[:, 0], forecast_mean[:, 0]+1.96*forecast_std[:, 0],
+        plt.fill_between(x_pred, forecast_mean[:, 0] - 1.96 * forecast_std[:, 0],
+                         forecast_mean[:, 0] + 1.96 * forecast_std[:, 0],
                          alpha=0.2, edgecolor='#CC4F1B', facecolor='#FF9848', label='95%-PI')
         plt.legend()
         plt.show()
-    print('Mean validation MSE:', validation_mse.mean())
     forecast_mse = sliding_window_mse(forecast_mean, data[gan.window_size:], gan.forecasting_horizon)
     print('Mean forecast MSE:', forecast_mse.mean())
-    print('Forecast MSE:',forecast_mse)
+    print('Forecast MSE:', forecast_mse)
     forecast_smape = sliding_window_smape(forecast_mean, data[gan.window_size:], gan.forecasting_horizon)
     print('Mean forecast SMAPE:', forecast_smape.mean())
     print('Forecast SMAPE:', forecast_smape)
@@ -163,28 +150,20 @@ def test_model(gan, data, validation_mse, plot=True):
                                             lower_limits=np.quantile(forecast, q=0.025, axis=-1),
                                             forecast_horizon=gan.forecasting_horizon)
     coverage_80_2 = sliding_window_coverage(actual_values=data[gan.window_size:],
-                                            upper_limits=forecast_mean + 1.28*total_uncertainty,
-                                            lower_limits=forecast_mean - 1.28*total_uncertainty,
+                                            upper_limits=forecast_mean + 1.28 * total_uncertainty,
+                                            lower_limits=forecast_mean - 1.28 * total_uncertainty,
                                             forecast_horizon=gan.forecasting_horizon).mean()
     coverage_95_2 = sliding_window_coverage(actual_values=data[gan.window_size:],
                                             upper_limits=forecast_mean + 1.96 * total_uncertainty,
                                             lower_limits=forecast_mean - 1.96 * total_uncertainty,
                                             forecast_horizon=gan.forecasting_horizon)
+
     width_80_1 = np.quantile(forecast, q=0.9, axis=-1)-np.quantile(forecast, q=0.1, axis=-1)
     width_95_1 = np.quantile(forecast, q=0.975, axis=-1)-np.quantile(forecast, q=0.025, axis=-1)
     width_80_2 = 2*1.28*total_uncertainty
     width_95_2 = 2*1.96*total_uncertainty
+
     if plot:
-        print('80%-prediction interval coverage - Mean:', coverage_80_1.mean(),
-              '\n Forecast horizon:', coverage_80_1)
-        print('95%-prediction interval coverage - Mean:', coverage_95_1.mean(),
-              '\n Forecast horizon:', coverage_95_1)
-
-        print('80%-prediction interval coverage - Mean:',coverage_80_2.mean(),
-              '\n Forecast horizon:', coverage_80_2)
-        print('95%-prediction interval coverage - Mean:',coverage_95_2.mean(),
-              '\n Forecast horizon:', coverage_95_2)
-
         plot_results(sliding_window_mse(forecast_mean, data[gan.window_size:], gan.forecasting_horizon),
                      label='Forecast MSE', title='Mean Squared Forecast Error', y_label='MSE')
         plot_results(sliding_window_coverage(actual_values=data[gan.window_size:],
@@ -203,40 +182,38 @@ def test_model(gan, data, validation_mse, plot=True):
 
 def pipeline():
     cfg = load_config_file('config\\config.yml')
-    forecast_mse_list, forecast_smape_list, validation_mse_list = [], [], []
+    forecast_mse_list, forecast_smape_list = [], []
     width_80_1_list, width_95_1_list, width_80_2_list, width_95_2_list = [], [], [], []
     coverage_80_1_list, coverage_95_1_list, coverage_80_2_list, coverage_95_2_list = [], [], [], []
     for i in range(1):
         gan = configure_model(cfg=cfg['gan'])
         train, test = load_data(cfg=cfg['data'], window_size=gan.window_size)
-        trained_gan, validation_mse = train_gan(gan=gan, data=train, epochs=cfg['gan']['epochs'],
-                                                batch_size=cfg['gan']['batch_size'], verbose=0)
+        trained_gan = train_gan(gan=gan, data=train, epochs=cfg['gan']['epochs'],
+                                batch_size=cfg['gan']['batch_size'], verbose=0)
         forecast_mse, forecast_smape, coverage_80_1, coverage_95_1, coverage_80_2, coverage_95_2, width_80_1, \
-            width_95_1, width_80_2, width_95_2 = test_model(gan=trained_gan, data=test, validation_mse=validation_mse, plot=False)
-        forecast_mse_list.append(forecast_mse), forecast_smape_list.append(forecast_smape), validation_mse_list.append(validation_mse)
+            width_95_1, width_80_2, width_95_2 = test_model(gan=trained_gan, data=test, plot=False)
+        forecast_mse_list.append(forecast_mse), forecast_smape_list.append(forecast_smape)
         coverage_80_1_list.append(coverage_80_1), coverage_95_1_list.append(coverage_95_1)
         coverage_80_2_list.append(coverage_80_2), coverage_95_2_list.append(coverage_95_2)
         width_80_1_list.append(width_80_1), width_95_1_list.append(width_95_1)
         width_80_2_list.append(width_80_2), width_95_2_list.append(width_95_2)
 
-    print('Mean validation MSE:', np.mean(np.mean(validation_mse_list, axis=0)))
     print('Mean forecast MSE:', np.mean(np.mean(forecast_mse_list, axis=0)))
-    print('Forecast MSE:', np.mean(np.array(forecast_mse_list), axis=0))
+    print('Forecast MSE:', np.mean(forecast_mse_list, axis=0))
     print('Mean forecast SMAPE:', np.mean(np.mean(forecast_smape_list, axis=0)))
-    print('Forecast SMAPE:', np.mean(np.array(forecast_smape_list), axis=0))
+    print('Forecast SMAPE:', np.mean(forecast_smape_list, axis=0))
     print('80%-prediction interval coverage - Mean:', np.mean(np.mean(coverage_80_1_list, axis=0)),
           ', width:', np.mean(width_80_1_list),
-          '\n Forecast horizon:', np.mean(np.array(coverage_80_1_list), axis=0))
-    print('95%-prediction interval coverage - Mean:',  np.mean(np.mean(coverage_95_1_list, axis=0)),
+          '\n Forecast horizon:', np.array(coverage_80_1_list).mean(axis=0))
+    print('95%-prediction interval coverage - Mean:', np.mean(np.mean(coverage_95_1_list, axis=0)),
           ', width:', np.mean(width_95_1_list),
-          '\n Forecast horizon:', np.mean(np.array(coverage_95_1_list), axis=0))
-
+          '\n Forecast horizon:', np.array(coverage_95_1_list).mean(axis=0))
     print('80%-prediction interval coverage - Mean:', np.mean(np.mean(coverage_80_2_list, axis=0)),
           ', width:', np.mean(width_80_2_list),
-          '\n Forecast horizon:', np.mean(np.array(coverage_80_2_list), axis=0))
+          '\n Forecast horizon:', np.array(coverage_80_2_list).mean(axis=0))
     print('95%-prediction interval coverage - Mean:', np.mean(np.mean(coverage_95_2_list, axis=0)),
           ', width:', np.mean(width_95_2_list),
-          '\n Forecast horizon:', np.mean(np.array(coverage_95_2_list), axis=0))
+          '\n Forecast horizon:', np.array(coverage_95_2_list).mean(axis=0))
 
 
 if __name__ == '__main__':
---
+++ models/hybrid_gan/ESWGAN.py
@@ -0,0 +1,290 @@
+import numpy as np
+from keras import Model
+from keras.layers import *
+from keras.optimizers import RMSprop
+from keras import backend
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.holtwinters import ExponentialSmoothing, HoltWintersResults
+
+import tensorflow as tf
+from tqdm import tqdm
+
+from models.feed_forward_gan.GAN import GAN
+from utility.ClipConstraint import ClipConstraint
+from utility.split_data import split_sequence
+
+
+class ESWGAN(GAN):
+    def __init__(self, cfg):
+        GAN.__init__(self, cfg)
+        self.plot_rate = cfg['plot_rate']
+        self.plot_folder = 'ESWGAN'
+        self.window_size = cfg['window_size']
+        self.forecasting_horizon = cfg['forecast_horizon']
+        self.recurrent_forecasting = cfg['recurrent_forecasting']
+        if self.recurrent_forecasting:
+            self.output_size = 1
+        else:
+            self.output_size = self.forecasting_horizon
+
+        self.noise_vector_size = cfg['noise_vector_size']  # Try larger vector
+        self.discriminator_epochs = cfg['discriminator_epochs']
+        self.mc_forward_passes = cfg['mc_forward_passes']
+
+        self.optimizer = RMSprop(lr=cfg['learning_rate'])
+        self.loss_function = self.wasserstein_loss
+        self.exponential_smoothing = None
+
+    def wasserstein_loss(self, y_true, y_pred):
+        return backend.mean(y_true * y_pred)
+
+    def build_generator(self):
+
+        noise_shape = (self.noise_vector_size,)
+
+        historic_shape = (self.window_size,)
+
+        noise_inp = Input(shape=noise_shape)
+        historic_inp = Input(shape=historic_shape)
+
+        x = Concatenate(axis=1)([historic_inp, noise_inp])
+        x = Dense(16)(x)
+        #x = Dense(16)(noise_inp)
+        x = ReLU()(x)
+        x = Dense(16)(x)
+        x = ReLU()(x)
+        # x = BatchNormalization()(x)
+        # x = Dense(16)(x)
+        # x = ReLU()(x)
+        # x = BatchNormalization()(x)
+
+        prediction = Dense(1)(x)
+
+        model = Model(inputs=[historic_inp, noise_inp], outputs=prediction)
+        model.summary()
+        return model
+
+    def build_discriminator(self):
+        historic_shape = (self.window_size,)
+        future_shape = (self.output_size,)
+
+        historic_inp = Input(shape=historic_shape)
+        future_inp = Input(shape=future_shape)
+
+        x = Concatenate(axis=1)([historic_inp, future_inp])
+
+        const = ClipConstraint(0.1)
+
+        x = Dense(64, kernel_constraint=const)(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = Dense(64, kernel_constraint=const)(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        # x = Dropout(0.4)(x)
+        validity = Dense(1)(x)
+
+        model = Model(inputs=[historic_inp, future_inp], outputs=validity)
+        model.summary()
+
+        return model
+
+    def build_model(self):
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss=self.loss_function, optimizer=self.optimizer, metrics=['accuracy'])
+
+        # Build and compile the generator
+        self.generator = self.build_generator()
+        self.generator.compile(loss=self.loss_function, optimizer=self.optimizer)
+
+        # The generator takes noise as input and generated forecasts
+        z = Input(shape=(self.noise_vector_size,))
+        time_series = Input(shape=(self.window_size,))
+
+        forecast = self.generator([time_series, z])
+        # For the combined model we will only train the generator
+        frozen_discriminator = Model(inputs=self.discriminator.inputs, outputs=self.discriminator.outputs)
+        frozen_discriminator.trainable = False
+        # self.discriminator.trainable = False
+
+        # Layer that add a dimension as the last axis
+        self.expand_dims = Lambda(lambda x: tf.keras.backend.expand_dims(x, axis=-1))
+
+        # The valid takes generated images as input and determines validity
+        print(time_series.shape, forecast.shape)
+        valid = frozen_discriminator([time_series, forecast])
+
+        # The combined model  (stacked generator and discriminator) takes
+        # noise as input => generates images => determines validity
+        self.combined = Model(inputs=[time_series, z], outputs=valid)
+        self.combined.compile(loss=self.loss_function, optimizer=self.optimizer)
+
+    def _get_labels(self, batch_size, real=True):
+        if real:
+            return np.ones((batch_size, 1))
+        else:
+            return -np.ones((batch_size, 1))
+
+    def fit_es(self, train):
+        # scaler = MinMaxScaler(feature_range=(10 ** (-10), 1))
+        # train = np.array(x[:self.window_size], y)
+        print(train.shape)
+        trends = [None, 'add', 'add_damped', 'mul']
+        seasons = [None, 'add', 'mul']
+        best_model_parameters = [None, None, False]  # trend, season, damped
+        best_aicc = np.inf
+        for trend in trends:
+            for season in seasons:
+                if trend == 'add_damped':
+                    trend = 'add'
+                    damped = True
+                else:
+                    damped = False
+                model_es = ExponentialSmoothing(train, seasonal_periods=12,
+                                                trend=trend, seasonal=season,
+                                                damped=damped)
+                model_es = model_es.fit(optimized=True)
+                if model_es.aicc < best_aicc:
+                    best_model_parameters = [trend, season, damped]
+                    best_aicc = model_es.aicc
+        model_es = ExponentialSmoothing(train, seasonal_periods=12,
+                                        trend=best_model_parameters[0], seasonal=best_model_parameters[1],
+                                        damped=best_model_parameters[2])
+        model_es = model_es.fit(optimized=True)
+
+        print(model_es.params)
+        print('ETS: T=', best_model_parameters[0], ', S=', best_model_parameters[1], ', damped=',
+              best_model_parameters[2])
+        print('AICc', model_es.aicc)
+        self.exponential_smoothing = model_es
+        return self.exponential_smoothing
+
+    def fit_gan(self, x, y, epochs=1, batch_size=32, verbose=1):
+        half_batch = int(batch_size / 2)
+        forecast_mse = np.zeros(epochs)
+        G_loss = np.zeros(epochs)
+        D_loss = np.zeros(epochs)
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            for d_epochs in range(max(1, self.discriminator_epochs)):
+                # Select a random half batch of images
+                idx = np.random.randint(0, x.shape[0], half_batch)
+                historic_time_series = x[idx]
+                future_time_series = y[idx]
+
+                generator_noise = self._generate_noise(half_batch)
+
+                # Generate a half batch of new images
+                gen_forecasts = self.generator.predict([historic_time_series, generator_noise])
+
+                # Train the discriminator
+                d_loss_real = self.discriminator.train_on_batch([historic_time_series, future_time_series],
+                                                                self._get_labels(batch_size=half_batch, real=True))
+                d_loss_fake = self.discriminator.train_on_batch([historic_time_series,
+                                                                gen_forecasts],
+                                                                self._get_labels(batch_size=half_batch, real=False))
+                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            generator_noise = self._generate_noise(batch_size)
+
+            # The generator wants the discriminator to label the generated samples
+            # as valid (ones)
+            valid_y = np.array([1] * batch_size)
+
+            idx = np.random.randint(0, x.shape[0], batch_size)
+            historic_time_series = x[idx]
+            # Train the generator
+            g_loss = self.combined.train_on_batch([historic_time_series, generator_noise], valid_y)
+
+            # Measure forecast MSE of generator
+            forecast_mse[epoch] = mean_squared_error(future_time_series, gen_forecasts)
+            # kl_divergence[epoch] = sum(self.kl_divergence(future_time_series[:, i, 0], gen_forecasts[:, i])
+            #                           for i in range(self.forecasting_horizon))/self.forecasting_horizon
+            G_loss[epoch] = g_loss
+            D_loss[epoch] = d_loss[0]
+            # Plot the progress
+            if verbose == 1:
+                print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f, forecast mse: %f]" %
+                      (epoch, d_loss[0], 100 * d_loss[1], g_loss, forecast_mse[epoch]))
+            # print("KL-divergence: ", kl_divergence[epoch])
+
+            if epoch % self.plot_rate == 0 and verbose == 1:
+                self.plot_distributions(future_time_series, gen_forecasts,
+                                        f'ims/' + self.plot_folder + f'/epoch{epoch:03d}.png')
+        return {'mse': forecast_mse, 'G_loss': G_loss, 'D_loss': D_loss, 'Accuracy': 100 * d_loss[1]}
+
+    def fit(self, x, y, epochs=1, batch_size=32, verbose=1):
+        # fit Exponential Smoothing
+        train = np.concatenate([x[0, :, 0], y[:, 0, 0]])
+        exponential_model = self.fit_es(train)
+        # transform data
+        pred_es = exponential_model.fittedvalues
+
+        es_transform = train-pred_es
+        es_transform = np.expand_dims(es_transform, axis=-1)
+        x_train, y_train = split_sequence(es_transform, self.window_size, self.output_size)
+
+        # Fit GAN
+        self.fit_gan(x_train[:, :, 0],  y_train[:, :, 0], epochs=epochs, batch_size=batch_size, verbose=1)
+        pred_gan = self.forecast(x_train)
+
+        # Combine predictions
+        pred = np.expand_dims(pred_es[self.window_size:], axis=-1) + pred_gan
+        # Print training mse
+        print('ES MSE:', mean_squared_error(y[:, :, 0], pred_es[self.window_size:]))
+        print('GAN MSE:', mean_squared_error(y_train[:, :, 0], pred_gan))
+        print('Mean squared error:', mean_squared_error(y[:, :, 0], pred))
+
+    def forecast(self, x):
+        # forecast exponential smoothing
+        es_forecasts = self.exponential_smoothing.forecast(steps=x.shape[0])
+
+        forecast = np.zeros([x.shape[0], self.mc_forward_passes, self.output_size])
+        for i in tqdm(range(x.shape[0])):
+            generator_noise = self._generate_noise(batch_size=self.mc_forward_passes)
+            x_input = np.vstack([np.expand_dims(x[i]-es_forecasts[i], axis=0)] * self.mc_forward_passes)
+            forecast[i] = self.generator.predict([x_input[:, :, 0], generator_noise])
+        return forecast.mean(axis=1)
+
+    def recurrent_forecast(self, time_series):
+        time_series = np.vstack([time_series] * self.mc_forward_passes)
+        x_input = np.zeros([self.mc_forward_passes, self.window_size + self.forecasting_horizon, 1])
+        x_input[:, :self.window_size] = time_series
+        for i in range(self.forecasting_horizon):
+            generator_noise = self._generate_noise(batch_size=self.mc_forward_passes)
+            x_input[:, self.window_size+i] = self.generator.predict([x_input[:, i:self.window_size+i, 0], generator_noise])
+        return x_input[:, -self.forecasting_horizon:].transpose()[0]
+
+    def monte_carlo_forecast(self, data, steps=1, plot=False):
+        # forecast ES
+        es_series = np.expand_dims(self.exponential_smoothing.forecast(steps=steps+self.window_size+self.forecasting_horizon), axis=-1)
+        es_series = np.expand_dims(es_series, axis=0)
+
+        data = np.expand_dims(data, axis=0)
+        print(data.shape)
+
+        time_series = data - es_series[:, :-self.forecasting_horizon]
+        print(time_series.shape)
+        print(es_series.shape)
+
+        es_forecasts = np.zeros([steps, self.forecasting_horizon, 1])
+        gan_forecast = np.zeros([steps, self.forecasting_horizon, self.mc_forward_passes])
+        for i in tqdm(range(steps)):
+            # forecast ES
+            es_forecasts[i] = es_series[:, self.window_size + i:self.window_size + i+self.forecasting_horizon]
+            if self.recurrent_forecasting:
+                gan_forecast[i] = self.recurrent_forecast(time_series[:, i:self.window_size + i])
+            else:
+                generator_noise = self._generate_noise(batch_size=self.mc_forward_passes)
+                x_input = np.vstack([time_series[:, i:self.window_size + i]]*self.mc_forward_passes)
+                gan_forecast[i] = self.generator.predict([x_input, generator_noise]).transpose()
+        forecast = gan_forecast + np.dstack([es_forecasts]*self.mc_forward_passes)
+        return forecast
