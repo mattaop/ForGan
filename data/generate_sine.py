fixed some scaling issues

data/generate_sine.py
@@ -1,31 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+import scipy.stats
 
 
 def generate_sine_data(num_points=2000, plot=False):
-    noise_distribution = 'gaussian'
+    np.random.seed(0)
     x = np.linspace(1, num_points, num_points)
-    if noise_distribution.lower() in ['gaussian', 'normal']:
-        print('Noise distribution: Gaussian')
-        noise = np.random.normal(0, 0.1, num_points)
-    elif noise_distribution.lower() == 'uniform':
-        print('Noise distribution: Uniform')
-        noise = np.random.uniform(-0.2, 0.2, num_points)
-    elif noise_distribution.lower() == 'gamma':
-        print('Noise distribution: Gamma')
-        noise = np.random.gamma(2, 2, num_points)/4
-    elif noise_distribution.lower() == 'bimodal':
-        print('Noise distribution: Bimodal')
-        y1 = np.random.normal(-0.3, 0.1, num_points)
-        y2 = np.random.normal(0.3, 0.1, num_points)
-        w = np.random.binomial(1, 0.5, num_points)  # 50:50 random choice
-        noise = w * y1 + (1 - w) * y2
-    else:
-        print('Noise distribution: None')
-        noise = np.zeros(num_points)
+    mean = 0
+    std = 0.1
+    noise = np.random.normal(mean, std, num_points)
 
-    y = np.sin(x*np.pi/6) + noise
+    y = np.sin(x*np.pi/6)/(scipy.stats.norm.ppf(0.75)*2) + noise
     if plot:
         plt.hist(noise, color='blue', edgecolor='black',
                  bins=int(50), density=True)
@@ -35,8 +22,9 @@ def generate_sine_data(num_points=2000, plot=False):
         plt.axvline(noise.mean(), color='b', linewidth=1)
         plt.show()
     print('Noise standard deviation:', noise.std())
+
     return np.expand_dims(y, axis=-1)
 
 
 if __name__ == '__main__':
-    print(generate_sine_data(50))
+    print(generate_sine_data(500))

plot_results.py
@@ -5,23 +5,25 @@ import matplotlib.pyplot as plt
 show_plot = True
 
 
-def read_training_files(file_name, training):
-    if training:
+def read_training_files(file_name, data):
+    if data == 'training':
         df = pd.read_csv(file_name + '/training_results.txt', header=0)
+    elif data == 'validation':
+        df = pd.read_csv(file_name + '/validation_results.txt', header=0)
     else:
         df = pd.read_csv(file_name + '/test_results.txt', header=0)
     return df
 
 
-def plot_results(save_file, model_paths, file_labels, title, x_label, y_label, value='mse', training=True, plot_rate=1):
+def plot_results(save_file, model_paths, file_labels, title, x_label, y_label, value='mse', data='training', plot_rate=1):
     colormap = plt.cm.get_cmap('RdBu', len(model_paths))
     print(colormap)
     plt.figure()
     for model_paths, file_label in zip(model_paths, file_labels):
-        df = read_training_files(model_paths, training)[value]
+        df = read_training_files(model_paths, data)[value]
         t = np.linspace(1, len(df) * plot_rate, len(df))
         plt.plot(t, df, label=file_label)
-    if value == 'mse' and training:
+    if value == 'mse' and data == 'training':
         plt.yscale("log")
     plt.title(title)
     plt.xlabel(x_label)
@@ -34,25 +36,40 @@ def plot_results(save_file, model_paths, file_labels, title, x_label, y_label, v
 
 def plot_training_results(model_paths, file_labels, title, save_file='Train', plot_rate=1):
     plot_results(save_file=save_file + '_train_mse', model_paths=model_paths, file_labels=file_labels, title=title[0],
-                 x_label='Epochs', y_label='Mean Squared Error (MSE)', value='mse', training=True, plot_rate=plot_rate)
+                 x_label='Epochs', y_label='Mean Squared Error (MSE)', value='mse', data='training', plot_rate=plot_rate)
     plot_results(save_file=save_file + '_train_80', model_paths=model_paths, file_labels=file_labels, title=title[1],
-                 x_label='Epochs', y_label='Coverage', value='coverage_80', training=True, plot_rate=plot_rate)
+                 x_label='Epochs', y_label='Coverage', value='coverage_80',  data='training', plot_rate=plot_rate)
     plot_results(save_file=save_file + '_train_95', model_paths=model_paths, file_labels=file_labels, title=title[2],
-                 x_label='Epochs', y_label='Coverage', value='coverage_95', training=True, plot_rate=plot_rate)
+                 x_label='Epochs', y_label='Coverage', value='coverage_95', data='training', plot_rate=plot_rate)
+
+
+def plot_validation_results(model_paths, file_labels, title, save_file='Validation'):
+    plot_results(save_file=save_file + '_validation_mse', model_paths=model_paths, file_labels=file_labels, title=title[0],
+                 x_label='Forecast horizon', y_label='Mean Squared Error (MSE)', value='mse',  data='validation',
+                 plot_rate=1)
+    plot_results(save_file=save_file + '_validation_smape', model_paths=model_paths, file_labels=file_labels, title=title[1],
+                 x_label='Forecast horizon', y_label='Symmetric Mean Absolute Percentage Error (sMAPE)', value='smape',
+                 data='validation', plot_rate=1)
+    plot_results(save_file=save_file + '_validation_80', model_paths=model_paths, file_labels=file_labels, title=title[2],
+                 x_label='Forecast horizon', y_label='Coverage', value='coverage_80', data='validation',
+                 plot_rate=1)
+    plot_results(save_file=save_file + '_validation_95', model_paths=model_paths, file_labels=file_labels, title=title[3],
+                 x_label='Forecast horizon', y_label='Coverage', value='coverage_95', data='validation',
+                 plot_rate=1)
 
 
 def plot_test_results(model_paths, file_labels, title, save_file='Test'):
     plot_results(save_file=save_file + '_test_mse', model_paths=model_paths, file_labels=file_labels, title=title[0],
-                 x_label='Forecast horizon', y_label='Mean Squared Error (MSE)', value='mse', training=False,
+                 x_label='Forecast horizon', y_label='Mean Squared Error (MSE)', value='mse',  data='test',
                  plot_rate=1)
     plot_results(save_file=save_file + '_test_smape', model_paths=model_paths, file_labels=file_labels, title=title[1],
                  x_label='Forecast horizon', y_label='Symmetric Mean Absolute Percentage Error (sMAPE)', value='smape',
-                 training=False, plot_rate=1)
+                 data='test', plot_rate=1)
     plot_results(save_file=save_file + '_test_80', model_paths=model_paths, file_labels=file_labels, title=title[2],
-                 x_label='Forecast horizon', y_label='Coverage', value='coverage_80', training=False,
+                 x_label='Forecast horizon', y_label='Coverage', value='coverage_80', data='test',
                  plot_rate=1)
     plot_results(save_file=save_file + '_test_95', model_paths=model_paths, file_labels=file_labels, title=title[3],
-                 x_label='Forecast horizon', y_label='Coverage', value='coverage_95', training=False,
+                 x_label='Forecast horizon', y_label='Coverage', value='coverage_95', data='test',
                  plot_rate=1)
 
 
@@ -82,28 +99,41 @@ def main():
                       'D$_{epochs}=10$',
                       # 'D$_{epochs}=15$', 'D$_{epochs}=20$', 'D$_{epochs}=50$'
                       ]
+    batch_size_paths = ['results/sine/recurrentgan/rnn_epochs_1500_D_epochs_5_batch_size_16_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                        'results/sine/recurrentgan/rnn_epochs_1500_D_epochs_5_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                        'results/sine/recurrentgan/rnn_epochs_1500_D_epochs_5_batch_size_64_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                        'results/sine/recurrentgan/rnn_epochs_1500_D_epochs_5_batch_size_128_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                        'results/sine/recurrentgan/rnn_epochs_1500_D_epochs_5_batch_size_256_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',]
+    batch_size_labels = ['Batch size = 16', 'Batch size = 32', 'Batch size = 64', 'Batch size = 128', 'Batch size = 256']
 
-    model_paths = d_epochs_paths
-    labels = d_epochs_label
+    model_paths = batch_size_paths
+    labels = batch_size_labels
     plot_training_results(model_paths=model_paths,
                           file_labels=labels,
-                          save_file='sine_d_epochs',
+                          save_file='sine_batch_size',
                           title=['Training Mean Squared Error',
                                  'Training 80% Prediction Interval Coverage',
                                  'Training 95% Prediction Interval Coverage'],
                           plot_rate=25)
 
-    model_paths = ['results/sine/arima',
-                   'results/sine/es',
-                   'results/sine/rnn/Epochs_1500_D_epochs_50_batch_size_64_noise_vec_50_lr_0.001000',
-                   'results/sine/recurrentgan/Epochs_1000_D_epochs_10_batch_size_64_noise_vec_100_lr_0.001000']
-    labels = ['ARIMA',
-              'Exponential Smoothing',
-              'MC Dropout',
-              'ForGAN']
+    # model_paths = ['results/sine/arima',
+    #                'results/sine/es',
+    #                'results/sine/rnn/Epochs_1500_D_epochs_50_batch_size_64_noise_vec_50_lr_0.001000',
+    #                'results/sine/recurrentgan/Epochs_1000_D_epochs_10_batch_size_64_noise_vec_100_lr_0.001000']
+    # labels = ['ARIMA',
+    #           'Exponential Smoothing',
+    #           'MC Dropout',
+    #           'ForGAN']
+    plot_validation_results(model_paths=model_paths,
+                            file_labels=labels,
+                            save_file='sine_batch_size',
+                            title=['Forecast Validation Mean Squared Error',
+                                   'Forecast Validation Symmetric Mean Absolute Percentage Error',
+                                   'Forecast Validation 80% Prediction Interval Coverage',
+                                   'Forecast Validation 95% Prediction Interval Coverage'])
     plot_test_results(model_paths=model_paths,
                       file_labels=labels,
-                      save_file='sine_compare_models',
+                      save_file='sine_batch_size',
                       title=['Forecast Mean Squared Error',
                              'Forecast Symmetric Mean Absolute Percentage Error',
                              'Forecast 80% Prediction Interval Coverage',

test_file.py
@@ -1,11 +1,19 @@
 import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 
-n = 100000
-mse = 0
-variables = np.zeros(n)
-for i in range(n):
-    variables[i] = np.random.normal(0, 0.1)
-    mse += 1/n*variables[i]**2
-print('MSE', mse)
-print('Std', np.std(variables))
-print('Var', np.var(variables))
+
+num_points = 10000000
+noise = np.random.normal(0, 0.1, num_points)
+x = np.linspace(1, num_points, num_points)
+y = np.sin(x*np.pi/6) + noise
+predictions = np.sin(x*np.pi/6)
+print(y.shape, predictions.shape)
+
+scaler = MinMaxScaler(feature_range=(10 ** (-10), 1))
+y = scaler.fit_transform(y.reshape(-1, 1))
+predictions = scaler.transform(predictions.reshape(-1, 1))
+
+
+print(mean_squared_error(y, predictions))

time_series_pipeline.py
@@ -24,7 +24,6 @@ sess = tf.Session(graph=tf.get_default_graph(), config=config)
 k.set_session(sess)
 
 import time
-import time
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.metrics import mean_squared_error, mutual_info_score, normalized_mutual_info_score
@@ -42,28 +41,16 @@ from utility.compute_statistics import *
 def configure_model(cfg):
     gan = get_gan(cfg)
     gan.build_model()
-    if cfg['model_name'].lower() in ['es', 'arima']:
-        paths = ['ims',
-                 'ims/' + gan.plot_folder,
-                 "results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower()
-                 ]
-    else:
-        paths = ['ims',
-                 'ims/' + gan.plot_folder,
-                 "results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() +
-                 "/Epochs_%d_D_epochs_%d_batch_size_%d_noise_vec_%d_lr_%f" %
-                 (cfg['epochs'], cfg['discriminator_epochs'], cfg['batch_size'], cfg['noise_vector_size'],
-                  cfg['learning_rate'])
-                 ]
+    paths = ['ims',
+             'ims/' + gan.plot_folder,
+             cfg['results_path']
+             ]
     for i in paths:
         if not os.path.exists(i):
             os.makedirs(i)
             print('Creating path:', i)
     if cfg['model_name'].lower() not in ['es', 'arima']:
-        write_config_file("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() +
-                          "/Epochs_%d_D_epochs_%d_batch_size_%d_noise_vec_%d_lr_%f" %
-                          (cfg['epochs'], cfg['discriminator_epochs'], cfg['batch_size'], cfg['noise_vector_size'],
-                           cfg['learning_rate']) + "/config.yml", cfg)
+        write_config_file(cfg['results_path'] + "/config.yml", cfg)
     return gan
 
 
@@ -80,7 +67,8 @@ def load_data(cfg, window_size):
     print('Data shape', data.shape)
     train = data[:-int(len(data)*cfg['test_split'])]
     test = data[-int(len(data)*cfg['test_split']+window_size):]
-    train, test = scale_data(train, test)
+    if not cfg['data_source'].lower() == 'sine':
+        train, test = scale_data(train, test)
     return train, test
 
 
@@ -226,13 +214,7 @@ def pipeline():
           ', width:', np.mean(width_95_2_list),
           '\n Forecast horizon:', np.mean(np.array(coverage_95_2_list), axis=0))
 
-    if cfg['model_name'].lower() in ['arima', 'es']:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() + "/test_results.txt")
-    else:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() +
-                     "/Epochs_%d_D_epochs_%d_batch_size_%d_noise_vec_%d_lr_%f/test_results.txt" %
-                     (cfg['epochs'], cfg['discriminator_epochs'], cfg['batch_size'],
-                      cfg['noise_vector_size'], cfg['learning_rate']))
+    file_name = cfg['results_path'] + "/test_results.txt"
     mse = np.mean(np.array(forecast_mse_list), axis=0)
     smap = np.mean(np.array(forecast_smape_list), axis=0)
     c_80 = np.mean(np.array(coverage_80_1_list), axis=0)

time_series_pipeline_baseline.py
@@ -91,13 +91,7 @@ def pipeline():
           ', width:', np.mean(width_95_list),
           '\n Forecast horizon:', np.mean(np.array(coverage_95_list), axis=0))
 
-    if cfg['model_name'].lower() in ['arima', 'es']:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() + "/test_results.txt")
-    else:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() +
-                     "/Epochs_%d_D_epochs_%d_batch_size_%d_noise_vec_%d_lr_%f/test_results.txt" %
-                     (cfg['epochs'], cfg['discriminator_epochs'], cfg['batch_size'],
-                      cfg['noise_vector_size'], cfg['learning_rate']))
+    file_name = cfg['results_path'] + "/test_results.txt"
     mse = np.mean(np.array(forecast_mse_list), axis=0)
     smap = np.mean(np.array(forecast_smape_list), axis=0)
     c_80 = np.mean(np.array(coverage_80_list), axis=0)

time_series_pipeline_with_validation.py
@@ -49,9 +49,9 @@ def compute_validation_error(model, data):
     return validation_mse
 
 
-def train_model(model, data, epochs, batch_size=128, verbose=1):
+def train_model(model, data, epochs, cfg, batch_size=128, verbose=1):
     # Split data in training and validation set
-    train, val = data[:-int(len(data)*0.1)], data[-int(model.window_size+len(data)*0.1):]
+    train, val = data[:-int(len(data)*cfg['val_split'])], data[-int(model.window_size+len(data)*cfg['val_split']):]
 
     # Split training data into (x_t-l, ..., x_t), (x_t+1) pairs
     x_train, y_train = split_sequence(train, model.window_size, model.output_size)
@@ -60,10 +60,10 @@ def train_model(model, data, epochs, batch_size=128, verbose=1):
 
     validation_mse = compute_validation_error(model, val)
 
-    return model, validation_mse
+    return model, validation_mse, val
 
 
-def test_model(model, data, validation_mse, plot=True):
+def test_model(model, data, validation_mse, cfg, plot=True, file_name="/test_results.txt"):
     forecast = model.monte_carlo_forecast(data, steps=int(len(data)-model.window_size), plot=plot)  # steps x horizon x mc_forward_passes
     forecast_mean = forecast.mean(axis=-1)
     forecast_std = forecast.std(axis=-1)
@@ -103,8 +103,8 @@ def test_model(model, data, validation_mse, plot=True):
                                             forecast_horizon=model.forecasting_horizon)
     width_80_1 = np.mean(np.quantile(forecast, q=0.9, axis=-1)-np.quantile(forecast, q=0.1, axis=-1), axis=0)
     width_95_1 = np.mean(np.quantile(forecast, q=0.975, axis=-1)-np.quantile(forecast, q=0.025, axis=-1), axis=0)
-    width_80_2 =2*1.28* np.mean(total_uncertainty, axis=0)
-    width_95_2 = 2*1.96* np.mean(total_uncertainty, axis=0)
+    width_80_2 = 2*1.28 * np.mean(total_uncertainty, axis=0)
+    width_95_2 = 2*1.96 * np.mean(total_uncertainty, axis=0)
     print(coverage_80_1.shape)
     print(width_80_1.shape)
     if plot:
@@ -135,6 +135,27 @@ def test_model(model, data, validation_mse, plot=True):
                      y2_label='95% PI coverage',
                      title='Prediction Interval Coverage', y_label='Coverage')
 
+        file_path = cfg['results_path'] + file_name
+
+        mse = np.mean(forecast_mse, axis=0)
+        smap = np.mean(forecast_smape, axis=0)
+        std = np.mean(forecast_std, axis=0)
+        if cfg['model_name'].lower() == 'rnn':
+            c_80 = np.mean(coverage_80_2, axis=0)
+            c_95 = np.mean(coverage_95_2, axis=0)
+            w_80 = np.mean(width_80_2, axis=0)
+            w_95 = np.mean(width_95_2, axis=0)
+        else:
+            c_80 = np.mean(coverage_80_1, axis=0)
+            c_95 = np.mean(coverage_95_1, axis=0)
+            w_80 = np.mean(width_80_1, axis=0)
+            w_95 = np.mean(width_95_1, axis=0)
+
+        with open(file_path, "a") as f:
+            f.write("mse,smape,std,coverage_80,coverage_95,width_80,width_95\n")
+            for (mse, smap, std, c_80, c_95, w_80, w_95) in zip(mse, smap, std, c_80, c_95, w_80, w_95):
+                f.write("{0},{1},{2},{3},{4},{5},{6}\n".format(mse, smap, std, c_80, c_95, w_80, w_95))
+
     return forecast_mse, forecast_smape, coverage_80_1, coverage_95_1, coverage_80_2, coverage_95_2, width_80_1, width_95_1, width_80_2, width_95_2, forecast_std.mean(axis=0)
 
 
@@ -147,13 +168,16 @@ def pipeline():
         model = configure_model(cfg=cfg)
         train, test = load_data(cfg=cfg, window_size=model.window_size)
         start_time = time.time()
-        trained_model, validation_mse = train_model(model=model, data=train, epochs=cfg['epochs'],
-                                                    batch_size=cfg['batch_size'], verbose=0)
+        trained_model, validation_mse, val = train_model(model=model, data=train, epochs=cfg['epochs'], cfg=cfg,
+                                                         batch_size=cfg['batch_size'], verbose=0)
         training_time = time.time() - start_time
-        # test_model(gan=trained_gan, data=train, validation_mse=validation_mse, plot=False)
+        test_model(model=trained_model, data=val, validation_mse=validation_mse, cfg=cfg, plot=False,
+                   file_name="/validation_results.txt")
         forecast_mse, forecast_smape, coverage_80_1, coverage_95_1, coverage_80_2, coverage_95_2, width_80_1, \
             width_95_1, width_80_2, width_95_2, forecast_std = test_model(model=trained_model, data=test,
-                                                                          validation_mse=validation_mse, plot=False)
+                                                                          validation_mse=validation_mse, cfg=cfg,
+                                                                          plot=False, file_name="/test_results.txt")
+
         forecast_mse_list.append(forecast_mse), forecast_smape_list.append(forecast_smape)
         validation_mse_list.append(validation_mse)
         forecast_std_list.append(forecast_std)
@@ -196,32 +220,6 @@ def pipeline():
     print('95%-prediction interval coverage - Mean:', np.mean(np.mean(coverage_95_2_list, axis=0)),
           ', width:', np.mean(width_95_2_list),
           '\n Forecast horizon:', np.mean(np.array(coverage_95_2_list), axis=0))
-
-    if cfg['model_name'].lower() in ['arima', 'es']:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() + "/test_results.txt")
-    else:
-        file_name = ("results/" + cfg['data_source'].lower() + "/" + cfg['model_name'].lower() +
-                     "/Epochs_%d_D_epochs_%d_batch_size_%d_noise_vec_%d_lr_%f/test_results.txt" %
-                     (cfg['epochs'], cfg['discriminator_epochs'], cfg['batch_size'],
-                      cfg['noise_vector_size'], cfg['learning_rate']))
-    mse = np.mean(np.array(forecast_mse_list), axis=0)
-    smap = np.mean(np.array(forecast_smape_list), axis=0)
-    std = np.mean(forecast_std_list, axis=0)
-    if cfg['model_name'].lower() == 'rnn':
-        c_80 = np.mean(np.array(coverage_80_2_list), axis=0)
-        c_95 = np.mean(np.array(coverage_95_2_list), axis=0)
-        w_80 = np.mean(np.array(width_80_2_list), axis=0)
-        w_95 = np.mean(np.array(width_95_2_list), axis=0)
-    else:
-        c_80 = np.mean(np.array(coverage_80_1_list), axis=0)
-        c_95 = np.mean(np.array(coverage_95_1_list), axis=0)
-        w_80 = np.mean(np.array(width_80_1_list), axis=0)
-        w_95 = np.mean(np.array(width_95_1_list), axis=0)
-
-    with open(file_name, "a") as f:
-        f.write("mse,smape,std,coverage_80,coverage_95,width_80,width_95\n")
-        for (mse, smap, std, c_80, c_95, w_80, w_95) in zip(mse, smap, std, c_80, c_95, w_80, w_95):
-            f.write("{0},{1},{2},{3},{4},{5},{6}\n".format(mse, smap, std, c_80, c_95, w_80, w_95))
 
 
 if __name__ == '__main__':