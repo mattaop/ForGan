fixed bugs in scaling

print_all_results.py
@@ -2,24 +2,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import yaml
+
 
 from config.load_config import load_config_file
 
 
 def read_files(file):
-    cfg = load_config_file(file+'/config.yml')
+    with open(file+'/config.yml', 'r') as f:
+        cfg = yaml.load(f)
     df = pd.read_csv(file + '/test_results.txt', header=0)
     return df, cfg
 
 
 def print_results(folder):
-    print("Layer | D_epochs | Noise vector | Batch Size | G nodes | D nodes | MSE | MASE | 80c | 95c | ")
+    print("Layer | Epochs | D_epochs | Noise vector | Batch Size | G nodes | D nodes | MSE | MASE | 80c | 95c | ")
     sub_folders = [f.path for f in os.scandir(folder) if f.is_dir()]
     for sub_folder in sub_folders:
         try:
             df, cfg = read_files(sub_folder)
-            print(cfg['layers'] + "  |    %d    |     %d     |     %d     |    %d    |    %d   | %.4f |%.2f| %.2f| %.2f| "
-                  % (cfg['discriminator_epochs'], cfg['noise_vector_size'], cfg['batch_size'], cfg['generator_nodes'],
+            print(cfg['layers'] + "  |  %d  |    %d    |     %d     |     %d     |    %d    |    %d   | %.2f |%.2f| %.2f| %.2f| "
+                  % (cfg['epochs'], cfg['discriminator_epochs'], cfg['noise_vector_size'], cfg['batch_size'], cfg['generator_nodes'],
                      cfg['discriminator_nodes'], np.mean(df['mse'])*1, np.mean(df['mase'])*1,
                      np.mean(df['coverage_80'])*100, np.mean(df['coverage_95'])*100))
         except:
@@ -27,7 +30,7 @@ def print_results(folder):
 
 
 def main():
-    folder = 'results/sine/recurrentgan'
+    folder = 'results/oslo/recurrentgan/minmax'
     print_results(folder=folder)
 
 

print_results_to_table.py
@@ -6,16 +6,29 @@ show_plot = True
 
 
 def read_files(file_name):
-    df = pd.read_csv(file_name + '/validation_results.txt', header=0)
+    df = pd.read_csv(file_name.lower() + '/test_results.txt', header=0)
     return df
 
 
 def print_results(model_paths, labels, model_names):
     for model_paths, label, model_name in zip(model_paths, labels, model_names):
         df = read_files(model_paths)
-        print(model_name + " & " + label + " & $ %.4f $ & $ %.2f $ & $ %.3f $ & $ %.2f $  &  $ %.2f $ & $ %.3f $ & $ %.3f $\\\\ "
-              % (np.mean(df['mse'])*1, np.mean(df['smape'])*1, np.mean(df['mase'])*1, np.mean(df['coverage_80'])*100,
-                 np.mean(df['coverage_95'])*100, np.mean(df['width_80'])*1, np.mean(df['width_95'])*1))
+        scale = 1
+        std = np.mean(df['std'])
+        if model_name in ['ForGAN']:
+            scale = 2.3985472432543657
+        """
+        print(
+            model_name + " & " + label + " & $ %.4f $ & $ %.2f $ & $ %.3f $ & $ %.2f $  &  $ %.2f $ & $ %.3f $ & $ %.3f $\\\\ "
+            % (np.mean(df['mse']) * 1, np.mean(df['smape']) * 1, np.mean(df['mase']) * 1,
+               np.mean(df['coverage_80']) * 100, np.mean(df['coverage_95']) * 100, np.mean(df['width_80']) * scale,
+               np.mean(df['width_95']) * scale))
+        """
+        print(model_name + " & $ %.4f $ & $ %.2f $ & $ %.3f $ & $ %.3f $ & $ %.2f $  &  $ %.2f $ & $ %.3f $ & $ %.3f $\\\\ "
+              % (np.mean(df['mse'])*1, np.mean(df['smape'])*1, np.mean(df['mase'])*1, std*scale,
+                 np.mean(df['coverage_80'])*100, np.mean(df['coverage_95'])*100, np.mean(df['width_80'])*scale,
+                 np.mean(df['width_95'])*scale))
+
 
 
 def main():
@@ -29,23 +42,23 @@ def main():
                    'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_3_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000'
                    ]
     noise_label = ['1', '5', '10', '25', '50', '100']
-    d_epochs_label = ["1", "2", "3", "5", "10", "20"]
-    d_epochs_paths = ['results/sine/recurrentgan/Epochs_1500_D_epochs_1_batch_size_64_noise_vec_50_lr_0.001000',
-                      'results/sine/recurrentgan/Epochs_1500_D_epochs_2_batch_size_64_noise_vec_50_lr_0.001000',
-                      'results/sine/recurrentgan/Epochs_1500_D_epochs_3_batch_size_64_noise_vec_50_lr_0.001000',
-                      'results/sine/recurrentgan/Epochs_1500_D_epochs_5_batch_size_64_noise_vec_50_lr_0.001000',
-                      'results/sine/recurrentgan/Epochs_1500_D_epochs_10_batch_size_64_noise_vec_50_lr_0.001000',
-                      'results/sine/recurrentgan/Epochs_1500_D_epochs_20_batch_size_64_noise_vec_50_lr_0.001000']
+    d_epochs_label = ["1", "3", "5", "10", "20"]
+    d_epochs_paths = ['results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_1_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                      'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_3_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                      'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_5_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                      'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_10_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000',
+                      'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_20_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000'
+]
 
     compare_models = ['ARIMA', 'ES', 'MC Dropout', 'ForGAN']
     compare_model_paths = ['results/sine/arima',
                            'results/sine/es',
-                           'results/sine/rnn/Epochs_1500_D_epochs_50_batch_size_64_noise_vec_50_lr_0.001000',
-                           'results/sine/recurrentgan/Epochs_1000_D_epochs_10_batch_size_64_noise_vec_100_lr_0.001000']
-
-    model_paths = noise_paths
+                           'results/sine/rnn/minmax/rnn_epochs_2000_D_epochs_5_batch_size_32_noise_vec_100_gnodes_64_dnodes_64_loss_kl_lr_0.001000',
+                           'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_5_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000'
+                           ]
+    model_paths = compare_model_paths
     labels = noise_label
-    model_names = forgan
+    model_names = compare_models
     print_results(model_paths=model_paths,
                   labels=labels,
                   model_names=model_names,)

test_models.py
@@ -63,7 +63,7 @@ def train_model(model, data, cfg):
     return model, validation_mse, val
 
 
-def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True, file_name="/test_results.txt"):
+def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True, file_name="/test_results.txt", minmax=None):
     forecast = model.monte_carlo_forecast(data, steps=int(len(data)-model.window_size), plot=plot)  # steps x horizon x mc_forward_passes
     forecast_mean = forecast.mean(axis=-1)
     forecast_std = forecast.std(axis=-1)
@@ -147,16 +147,24 @@ def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True,
     mase = forecast_mase
     if cfg['model_name'].lower() == 'rnn':
         std = total_uncertainty.mean(axis=0)
-        c_80 = coverage_80_2
-        c_95 = coverage_95_2
-        w_80 = width_80_2
-        w_95 = width_95_2
+        coverage_80 = coverage_80_2
+        coverage_95 = coverage_95_2
+        width_80 = width_80_2
+        width_95 = width_95_2
+        if minmax:
+            std *= minmax
+            width_80 *= minmax
+            width_95 *= minmax
     else:
-        std = np.mean(forecast_std, axis=0)
-        c_80 = coverage_80_1
-        c_95 = coverage_95_1
-        w_80 = width_80_1
-        w_95 = width_95_1
+        std = forecast_std.mean(axis=0)
+        coverage_80 = coverage_80_1
+        coverage_95 = coverage_95_1
+        width_80 = width_80_1
+        width_95 = width_95_1
+        if minmax:
+            std *= minmax
+            width_80 *= minmax
+            width_95 *= minmax
 
     with open(file_path, "a") as f:
         f.write("mse,smape,mase,std,coverage_80,coverage_95,width_80,width_95\n")
@@ -182,11 +190,19 @@ def pipeline(model_path, model_name):
         trained_model, validation_mse, val = train_model(model=model, data=train, cfg=cfg)
         training_time = time.time() - start_time
         test_model(model=trained_model, data=val, validation_mse=validation_mse, cfg=cfg, naive_error=naive_error,
-                   scaler=scaler, plot=False, file_name="/validation_results_" + model_name + ".txt")
+                   scaler=scaler, plot=False, file_name="/validation_results_" + model_name + ".txt",   minmax=(np.max(
+                                                                       scaler.inverse_transform(train)) - np.min(
+                                                                       scaler.inverse_transform(train))) /
+                                                                          (np.max(train) - np.min(train)))
         mse, smape, mase, std, c_80, c_95, w_80, w_95 = test_model(model=trained_model, data=test,
                                                                    validation_mse=validation_mse, cfg=cfg,
                                                                    naive_error=naive_error, scaler=scaler, plot=False,
-                                                                   file_name="/test_results" + model_name + ".txt")
+                                                                   file_name="/test_results" + model_name + ".txt",
+                                                                   minmax=(np.max(
+                                                                       scaler.inverse_transform(train)) - np.min(
+                                                                       scaler.inverse_transform(train))) /
+                                                                          (np.max(train) - np.min(train))
+                                                                   )
 
         forecast_mse_list.append(mse), forecast_smape_list.append(smape), forecast_mase_list.append(mase)
         validation_mse_list.append(validation_mse)
@@ -220,6 +236,6 @@ def pipeline(model_path, model_name):
 
 
 if __name__ == '__main__':
-    model_path = 'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_3_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000/'
-    model_name = 'generator_best_900.h5'
+    model_path = 'results/sine/recurrentgan/minmax/rnn_epochs_1500_D_epochs_5_batch_size_32_noise_vec_100_gnodes_16_dnodes_64_loss_kl_lr_0.001000/'
+    model_name = 'generator_1500.h5'
     pipeline(model_path, model_name)

time_series_pipeline.py
@@ -68,17 +68,15 @@ def load_data(cfg, window_size):
     train = data[:-int(len(data)*cfg['test_split'])]
     print(np.max(train)-np.min(train))
     test = data[-int(len(data)*cfg['test_split']+window_size):]
+    original_scale = np.max(train)-np.min(train)
     if cfg['model_name'].lower() == 'es':
         scaler = MinMaxScaler(feature_range=(1, 2))
         train = scaler.fit_transform(train)
         test = scaler.transform(test)
-    elif cfg['data_source'].lower() == 'sine':
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        train = scaler.fit_transform(train)
-        test = scaler.transform(test)
+        print(np.max(train), np.min(train))
     else:
-        train, test, scaler = scale_data(train, test)
-
+        train, test, scaler = scale_data(train, test, cfg)
+    print(original_scale/(np.max(train)-np.min(train)))
     """
     print(np.min(train), np.max(train))
     plt.plot(train)

time_series_pipeline_baseline.py
@@ -31,13 +31,13 @@ from time_series_pipeline import configure_model, load_data, train_model
 from utility.compute_statistics import *
 
 
-def test_model(model, data, naive_error, cfg, scaler, plot=True, file_name="/test_results.txt"):
+def test_model(model, data, naive_error, cfg, scaler, plot=True, file_name="/test_results.txt", minmax=None):
     forecast = model.monte_carlo_forecast(data, steps=int(len(data) - model.window_size), plot=plot)  # steps x horizon x mc_forward_passes
 
     forecast_mse = sliding_window_mse(scaler.inverse_transform(forecast),
                                       scaler.inverse_transform(data[model.window_size:]),
                                       model.forecasting_horizon)
-    forecast_std = np.sqrt(model.variance).mean(axis=0)
+    forecast_std = np.sqrt(model.variance)
 
     forecast_smape = sliding_window_smape(scaler.inverse_transform(forecast),
                                           scaler.inverse_transform(data[model.window_size:]),
@@ -46,6 +46,18 @@ def test_model(model, data, naive_error, cfg, scaler, plot=True, file_name="/tes
                                         scaler.inverse_transform(data[model.window_size:]),
                                         model.forecasting_horizon,
                                         naive_error)
+
+    if plot:
+        x_pred = np.linspace(model.window_size+1, len(data), len(data)-model.window_size)
+        plt.figure()
+        plt.plot(np.linspace(1, len(data), len(data)), data, label='Data')
+        plt.plot(x_pred, forecast[:, 0], label='Predictions')
+        plt.fill_between(x_pred, model.pred_int_80[:, 0, 0], model.pred_int_80[:, 0, 1],
+                         alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848', label='80%-PI')
+        plt.fill_between(x_pred, model.pred_int_95[:, 0, 1], model.pred_int_95[:, 0, 1],
+                         alpha=0.2, edgecolor='#CC4F1B', facecolor='#FF9848', label='95%-PI')
+        plt.legend()
+        plt.show()
 
     coverage_80 = sliding_window_coverage(actual_values=data[model.window_size:],
                                           upper_limits=model.pred_int_80[:, :, 1],
@@ -58,14 +70,18 @@ def test_model(model, data, naive_error, cfg, scaler, plot=True, file_name="/tes
 
     width_80 = np.mean(model.pred_int_80[:, :, 1] - model.pred_int_80[:, :, 0], axis=0)
     width_95 = np.mean(model.pred_int_95[:, :, 1] - model.pred_int_95[:, :, 0], axis=0)
-
+    if minmax:
+        forecast_std = forecast_std * minmax
+        width_80 = width_80 * minmax
+        width_95 = width_95 * minmax
     file_path = cfg['results_path'] + file_name
 
     with open(file_path, "a") as f:
-        f.write("mse,smape,mase,coverage_80,coverage_95,width_80,width_95\n")
-        for (mse, smape, mase, c_80, c_95, w_80, w_95) in zip(forecast_mse, forecast_smape, forecast_mase, coverage_80,
-                                                              coverage_95, width_80, width_95):
-            f.write("{0},{1},{2},{3},{4},{5},{6}\n".format(mse, smape, mase, c_80, c_95, w_80, w_95))
+        f.write("mse,smape,mase,std,coverage_80,coverage_95,width_80,width_95\n")
+        for (mse, smape, mase, std, c_80, c_95, w_80, w_95) in zip(forecast_mse, forecast_smape, forecast_mase,
+                                                                   forecast_std, coverage_80, coverage_95, width_80,
+                                                                   width_95):
+            f.write("{0},{1},{2},{3},{4},{5},{6},{7}\n".format(mse, smape, mase, std, c_80, c_95, w_80, w_95))
 
     return forecast_mse, forecast_smape, forecast_mase, forecast_std, coverage_80, coverage_95, width_80, width_95
 
@@ -85,7 +101,9 @@ def pipeline():
                                     batch_size=cfg['batch_size'], verbose=1)
         training_time = time.time() - start_time
         mse, smape, mase, std, c_80, c_95, w_80, w_95 = \
-            test_model(model=trained_model, data=test, naive_error=naive_error, scaler=scaler, cfg=cfg, plot=False)
+            test_model(model=trained_model, data=test, naive_error=naive_error, scaler=scaler, cfg=cfg, plot=True,
+                       minmax=(np.max(scaler.inverse_transform(train))-np.min(scaler.inverse_transform(train)))/
+                              (np.max(train)-np.min(train)))
         forecast_mse_list.append(mse), forecast_smape_list.append(smape), forecast_mase_list.append(mase)
         coverage_80_list.append(c_80), coverage_95_list.append(c_95)
         width_80_list.append(w_80), width_95_list.append(w_95)

time_series_pipeline_with_validation.py
@@ -63,7 +63,7 @@ def train_model(model, data, epochs, cfg, batch_size=128, verbose=1):
     return model, validation_mse, val
 
 
-def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True, file_name="/test_results.txt"):
+def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True, file_name="/test_results.txt", minmax=None):
     forecast = model.monte_carlo_forecast(data, steps=int(len(data)-model.window_size), plot=plot)  # steps x horizon x mc_forward_passes
     forecast_mean = forecast.mean(axis=-1)
     forecast_std = forecast.std(axis=-1)
@@ -147,13 +147,20 @@ def test_model(model, data, validation_mse, cfg, naive_error, scaler, plot=True,
         coverage_95 = coverage_95_2
         width_80 = width_80_2
         width_95 = width_95_2
+        if minmax:
+            std *= minmax
+            width_80 *= minmax
+            width_95 *= minmax
     else:
-        std = np.mean(forecast_std, axis=0)
+        std = forecast_std.mean(axis=0)
         coverage_80 = coverage_80_1
         coverage_95 = coverage_95_1
         width_80 = width_80_1
         width_95 = width_95_1
-    std = scaler.inverse_transform(std.reshape(-1, 1))
+        if minmax:
+            std *= minmax
+            width_80 *= minmax
+            width_95 *= minmax
 
     with open(file_path, "a") as f:
         f.write("mse,smape,mase,std,coverage_80,coverage_95,width_80,width_95\n")
@@ -180,11 +187,17 @@ def pipeline():
                                                          batch_size=cfg['batch_size'], verbose=0)
         training_time = time.time() - start_time
         test_model(model=trained_model, data=val, validation_mse=validation_mse, cfg=cfg, naive_error=naive_error,
-                   scaler=scaler, plot=False, file_name="/validation_results.txt")
+                   scaler=scaler, plot=False, file_name="/validation_results.txt", minmax=(np.max(
+                scaler.inverse_transform(train))-np.min(scaler.inverse_transform(train)))/(np.max(train)-np.min(train)))
         mse, smape, mase, std, c_80, c_95, w_80, w_95 = test_model(model=trained_model, data=test,
                                                                    validation_mse=validation_mse, cfg=cfg,
                                                                    naive_error=naive_error, scaler=scaler, plot=False,
-                                                                   file_name="/test_results.txt")
+                                                                   file_name="/test_results.txt",
+                                                                   minmax=(np.max(
+                                                                       scaler.inverse_transform(train)) - np.min(
+                                                                       scaler.inverse_transform(train))) /
+                                                                          (np.max(train) - np.min(train))
+                                                                   )
 
         forecast_mse_list.append(mse), forecast_smape_list.append(smape), forecast_mase_list.append(mase)
         validation_mse_list.append(validation_mse)